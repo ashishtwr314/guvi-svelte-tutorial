59883bda193ee052c36e83d7d1e73424ace181ec
ashishcollekt <ashish.tiwari@collekt.ai>
2025-07-03 13:34:18 +0530

    Basics of svelte

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  let count = 0;
+  function increment() {
+    count = count + 1;
+  }
+  function decrement() {
+    count = count - 1;
+  }
+</script>
+
+<h1>{count}</h1>
+<button on:click={increment}>Increment +</button>
+<button on:click={decrement}>Decrement -</button>
+
+<style>
+</style>
